a5cbb0795df79a808ffa0765c5bb78f1af0b55aa
Cedric Guerin <guerin.ced@gmail.com>
2017-03-17 19:30:13 +0100

    Add link to route in app component

--- src/components/App/app.jsx
+++ src/components/App/app.jsx
@@ -1,6 +1,17 @@
+import { Link } from 'react-router'
+
 const App = ({children}) => {
     return (
         <div>
+            <Link to="/">
+                <div style={{display: "inline", "margin-right": "10px"}}>Counter</div>
+            </Link>
+            <Link to="/hello-world">
+                <div style={{display: "inline", "margin-right": "10px"}}>Hellow World</div>
+            </Link>
+            <Link to="/not-existing-route">
+                <div style={{display: "inline"}}>Not-existing-route</div>
+            </Link>
             {children}
         </div>
     )
