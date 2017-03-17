d15cd8ad4c73ff9588bac63c66a05e73acb24c9e
Cedric Guerin <guerin.ced@gmail.com>
2017-03-17 13:27:21 +0100

    Hello World!

---
+++ README.md
@@ -0,0 +1,13 @@
+# React-Redux-Boilerplate
+
+Minimalist React-Redux application with webpack 2 build.
+
+## Setup for development
+```bash
+npm i
+npm start
+```
+## Access
+```
+localhost:3000/
+```
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    devtool: 'eval-source-map',
+    entry: [
+        // bundle the client for webpack-dev-server
+        // and connect to the provided endpoint
+        'webpack-dev-server/client?http://localhost:3000',
+
+        // bundle the client for hot reloading
+        // only- means to only hot reload for successful updates
+        'webpack/hot/only-dev-server',
+
+        // the entry point of our app
+        path.join(__dirname, 'src/index.js')
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        historyApiFallback: true,
+        port: 3000,
+        open: true,
+
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/, // JS ou JSX
+                loaders: ['babel-loader'],
+                include: path.join(__dirname, 'src')
+            }
+        ]
+    },
+    plugins: [
+
+        // Plugin to generate index.html from index.tpl.html
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src/index.tpl.html'),
+            filename: 'index.html',
+            inject: 'body'
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+
+        // Load and declare React as global variable (= window.react)
+        new webpack.ProvidePlugin({
+            React: 'react',
+        })
+    ]
+}
